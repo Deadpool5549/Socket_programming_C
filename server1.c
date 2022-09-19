b64c84a8e060b6388578384abe9e7cfadfcf198c
Deadpool5549 <67370921+Deadpool5549@users.noreply.github.com>
2022-09-19 14:56:12 +0530

    Add files via upload

---
+++ server1.c
@@ -0,0 +1,44 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+//this is a tcp server
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main()
+{
+    char servMessage[100]="hi ,!";
+    int servSocket;
+    servSocket=socket(AF_INET,SOCK_STREAM,0);
+
+    //define the server address
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_port=htons(9002);
+    serverAddr.sin_addr.s_addr=INADDR_ANY;
+
+    //bind soc to IP
+    bind(servSocket,(struct sockaddr*) &serverAddr,sizeof(serverAddr));
+
+    //call listen for listening for connections
+
+    listen(servSocket,5);
+
+    int client_socket;
+    //accept(server socket,Client address, Size of) returns int
+    client_socket=accept(servSocket,NULL,NULL);//both null cuz local machine
+
+    //now we can send and recv data
+    char client_name[30];
+    recv(client_socket,client_name,sizeof(client_name),0);
+    strcat(servMessage,client_name);
+
+    send(client_socket,servMessage,sizeof(servMessage),0);
+
+    //close it
+    close(servSocket);
+    return 0;
+}
